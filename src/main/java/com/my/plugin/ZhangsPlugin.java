2b16a2160568f59d8bc3cdd59ec28607dd7922e0
zhangsong <243279672@qq.com>
2019-01-24 11:25:43 +0800

    Initial commit

---
+++ src/main/java/com/my/plugin/ZhangsPlugin.java
@@ -0,0 +1,14 @@
+package com.my.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "zhangsplugin",defaultPhase = LifecyclePhase.PACKAGE)
+public class ZhangsPlugin extends AbstractMojo {
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        System.out.println("--------Hello My Plusgin----------");
+    }
+}
